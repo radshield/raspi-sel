1199905622eb4f3693e65971624d4d47c6205761
Haoda Wang <harry@h313.info>
2023-03-27 13:35:42 -0400

    Add base INA169 measurement information

---
+++ ina169_measurement.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import board, busio
+import psutil
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+def init_adc():
+  i2c = busio.I2C(board.SCL, board.SDA)
+  ads = ADS.ADS1115(i2c)
+  return AnalogIn(ads, ADS.P0)
+
+
+def main():
+  adc = init_adc()
+  print(psutil.cpu_percent(), psutil.virtual_memory().percent, adc.voltage)
+
+if __name__ == '__main__':
+  main()
