6e870a134250e0924ea5e64829dbe30613347bbe
Haoda Wang <harry@h313.info>
2023-04-05 21:31:14 -0400

    Add logging and use single mode

--- ina169_measurement.py
+++ ina169_measurement.py
@@ -1,19 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import board, busio
+import board
+import busio
+import datetime
 import psutil
+import time
+
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
+data_rate = 860
+
 def init_adc():
   i2c = busio.I2C(board.SCL, board.SDA)
-  ads = ADS.ADS1115(i2c, data_rate=860, mode=ADS.Mode.CONTINUOUS)
+  ads = ADS.ADS1115(i2c, data_rate=data_rate, mode=ADS.Mode.SINGLE)
   return AnalogIn(ads, ADS.P0)
 
+def main_loop(adc, filename='currents.log'):
+  log = open(filename, 'a')
 
-def main():
-  adc = init_adc()
-  print(psutil.cpu_percent(), psutil.virtual_memory().percent, adc.voltage)
+  while True:
+    log.write('time,cpu_percent,virtual_memory,adc_voltage')
+    log.write('{},{},{},{}'.format(str(datetime.datetime.now()),
+                                   psutil.cpu_percent(),
+                                   psutil.virtual_memory().percent,
+                                   adc.voltage))
+    time.sleep(1 / data_rate)
 
 if __name__ == '__main__':
-  main()
+  adc = init_adc()
+  main_loop(adc)
