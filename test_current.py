f34a45d8da5b64cff872aeb86b252ee52dd39fbc
Haoda Wang <harry@h313.info>
2023-04-14 10:57:36 -0400

    Clean up test_current.py

--- test_current.py
+++ test_current.py
@@ -1,35 +1,8 @@
 #!/usr/bin/env python3
 
-import argparse
-import board
-import busio
-import datetime
-import json
-import psutil
-import subprocess
-import time
-
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
-
-parser = argparse.ArgumentParser(description='Measure current with load on INA169.')
-parser.add_argument('-d', '--data-rate', metavar='N', dest='data_rate', default=60,
-                    help='number of measurements to record per second')
-parser.add_argument('input_file', type=str, nargs=1,
-                    help='input file for commands to run, as JSON')
-
-def init_adc():
-  i2c = busio.I2C(board.SCL, board.SDA)
-  ads = ADS.ADS1115(i2c, data_rate=860, mode=ADS.Mode.SINGLE)
-  return AnalogIn(ads, ADS.P1)
+from ina169_measurement import init_adc;
 
 if __name__ == '__main__':
-  args = parser.parse_args()
-
-  with open(args.input_file[0]) as f:
-    tests = json.load(f)['runs']
-
   adc = init_adc()
   while True:
-    print(adc.voltage)
-    print()
+    print('Voltage: {}\n'.format(adc.voltage))
