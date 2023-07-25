e5e2124ad21b66f4fd8bccd17795a9e14d1314e6
Haoda Wang <harry@h313.info>
2023-07-24 18:51:49 -0700

    Support 3 channels on INA3221

--- ina169_measurement.py
+++ ina169_measurement.py
@@ -18,42 +18,45 @@ parser.add_argument('-d', '--data-rate', metavar='N', dest='data_rate', default=
 parser.add_argument('input_file', type=str, nargs=1,
                     help='input file for commands to run, as JSON')
 
+
 def init_adc():
-  i2c = busio.I2C(board.SCL, board.SDA)
-  ads = ADS.ADS1115(i2c, data_rate=860, mode=ADS.Mode.SINGLE)
-  return AnalogIn(ads, ADS.P1)
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1115(i2c, data_rate=860, mode=ADS.Mode.SINGLE)
+    return AnalogIn(ads, ADS.P1)
+
 
 def main_loop(adc, tests, data_rate, out_file='currents.log'):
-  log = open(out_file, 'w')
-  log.write('time,cpu_percent_0,cpu_percent_1,cpu_percent_2,cpu_percent_3,' +
-            'virtual_memory,adc_voltage\n')
-  log.flush()
-  
-  for test in tests:
-    test_loads = []
-    for command in test['commands']:
-      test_loads.append(subprocess.Popen(command.split(' ')))
+    log = open(out_file, 'w')
+    log.write('time,cpu_percent_0,cpu_percent_1,cpu_percent_2,cpu_percent_3,' +
+              'virtual_memory,adc_voltage\n')
+    log.flush()
 
-    for i in range(test['run_time'] * data_rate):
-      log.write('{},{},{},{},{},{},{}\n'.format(str(datetime.datetime.now()),
-        100.0 - psutil.cpu_times_percent(percpu=True)[0].idle,
-        100.0 - psutil.cpu_times_percent(percpu=True)[1].idle,
-        100.0 - psutil.cpu_times_percent(percpu=True)[2].idle,
-        100.0 - psutil.cpu_times_percent(percpu=True)[3].idle,
-        psutil.virtual_memory().percent,
-        adc.voltage))
-      log.flush()
-      time.sleep(1 / data_rate)
+    for test in tests:
+        test_loads = []
+        for command in test['commands']:
+            test_loads.append(subprocess.Popen(command.split(' ')))
 
-    for test_load in test_loads:
-      if test_load is not None and test_load.poll() is None:
-        test_load.kill()
+        for i in range(test['run_time'] * data_rate):
+            log.write('{},{},{},{},{},{},{}\n'.format(str(datetime.datetime.now()),
+                                                      100.0 - psutil.cpu_times_percent(percpu=True)[0].idle,
+                                                      100.0 - psutil.cpu_times_percent(percpu=True)[1].idle,
+                                                      100.0 - psutil.cpu_times_percent(percpu=True)[2].idle,
+                                                      100.0 - psutil.cpu_times_percent(percpu=True)[3].idle,
+                                                      psutil.virtual_memory().percent,
+                                                      adc.voltage))
+            log.flush()
+            time.sleep(1 / data_rate)
+
+        for test_load in test_loads:
+            if test_load is not None and test_load.poll() is None:
+                test_load.kill()
+
 
 if __name__ == '__main__':
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  with open(args.input_file[0]) as f:
-    tests = json.load(f)['runs']
+    with open(args.input_file[0]) as f:
+        tests = json.load(f)['runs']
 
-  adc = init_adc()
-  main_loop(adc, tests, int(args.data_rate))
+    adc = init_adc()
+    main_loop(adc, tests, int(args.data_rate))
--- ina3221_measurement.py
+++ ina3221_measurement.py
@@ -6,7 +6,6 @@ import busio
 import datetime
 import json
 import psutil
-import subprocess
 import time
 
 from barbudor_ina3221.lite import INA3221
@@ -14,51 +13,45 @@ from barbudor_ina3221.lite import INA3221
 parser = argparse.ArgumentParser(description='Measure current with load and peripherals on INA3221.')
 parser.add_argument('-d', '--data-rate', metavar='N', dest='data_rate', default=100,
                     help='number of measurements to record per second')
-parser.add_argument('input_file', type=str, nargs=1,
-                    help='input file for commands to run, as JSON')
+parser.add_argument('-r', '--run-time', metavar='N', dest='run_time', default=100,
+                    help='number of seconds to record data')
 
-def init_monitor(channels=[0, 1]):
-  i2c = busio.I2C(board.SCL, board.SDA)
-  monitor = INA3221(i2c)
 
-  for channel in channels:
-    monitor.enable_channel(channel)
+def init_monitor(channels=[0, 1, 2]):
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ina3221 = INA3221(i2c)
 
-  return monitor
+    for channel in channels:
+        ina3221.enable_channel(channel)
 
-def main_loop(monitor, tests, channels=[0, 1], data_rate=100, out_file='currents.log'):
-  log = open(out_file, 'w')
-  log.write('time,cpu_percent_0,cpu_percent_1,cpu_percent_2,cpu_percent_3,' +
-            'virtual_memory,adc_voltage_all,adc_voltage_payload\n')
-  log.flush()
-  
-  for test in tests:
-    test_loads = []
-    for command in test['commands']:
-      test_loads.append(subprocess.Popen(command.split(' ')))
+    return ina3221
 
-    for i in range(test['run_time'] * data_rate):
-      log.write('{},{},{},{},{},{},{},{}\n'.format(str(datetime.datetime.now()),
-        100.0 - psutil.cpu_times_percent(percpu=True)[0].idle,
-        100.0 - psutil.cpu_times_percent(percpu=True)[1].idle,
-        100.0 - psutil.cpu_times_percent(percpu=True)[2].idle,
-        100.0 - psutil.cpu_times_percent(percpu=True)[3].idle,
-        psutil.virtual_memory().percent,
-        monitor.current(channels[0]),
-        monitor.current(channels[1])))
-      log.flush()
-      time.sleep(1 / data_rate)
 
-    for test_load in test_loads:
-      if test_load is not None and test_load.poll() is None:
-        test_load.kill()
+def main_loop(ina3221, run_time=60, channels=[0, 1, 2], data_rate=100, out_file='currents.log'):
+    log = open(out_file, 'w')
+    log.write('time,cpu_percent_0,cpu_percent_1,cpu_percent_2,cpu_percent_3,' +
+              'virtual_memory,adc_voltage_all,adc_voltage_payload,adc_voltage_peripherals\n')
+    log.flush()
+
+    for i in range(data_rate * run_time):
+        log.write('{},{},{},{},{},{},{},{},{}\n'.format(str(datetime.datetime.now()),
+                                                        100.0 - psutil.cpu_times_percent(percpu=True)[0].idle,
+                                                        100.0 - psutil.cpu_times_percent(percpu=True)[1].idle,
+                                                        100.0 - psutil.cpu_times_percent(percpu=True)[2].idle,
+                                                        100.0 - psutil.cpu_times_percent(percpu=True)[3].idle,
+                                                        psutil.virtual_memory().percent,
+                                                        ina3221.current(channels[0]),
+                                                        ina3221.current(channels[1]),
+                                                        ina3221.current(channels[2])))
+        log.flush()
+        time.sleep(1 / data_rate)
+
 
 if __name__ == '__main__':
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  with open(args.input_file[0]) as f:
-    tests = json.load(f)['runs']
+    with open(args.input_file[0]) as f:
+        tests = json.load(f)['runs']
 
-  monitor = init_monitor()
-  main_loop(monitor, tests, data_rate=int(args.data_rate))
-
+    monitor = init_monitor()
+    main_loop(monitor, tests, run_time=int(args.run_time), data_rate=int(args.data_rate))
