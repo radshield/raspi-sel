2d3d1763820d4632920a10400432490a323414ba
Harry <harry@h313.info>
2024-07-04 13:23:23 -0400

    Activate virtualenv in `run_monitor.sh`

--- run_monitor.sh
+++ run_monitor.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Switch to project directory and activate virtualenv
+cd /home/pi/latchup
+source ./venv/bin/activate
+
 # Record 1 minute of system data
 ./record input.csv 60
 
